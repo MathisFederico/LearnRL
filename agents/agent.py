ae2a63bfc1ae0523401cc8996238dcf1876f719a
Mathïs <mathfederico@gmail.com>
2020-02-16 20:04:26 +0100

    Indentation error fixed in Memory

--- agents/agent.py
+++ agents/agent.py
@@ -8,23 +8,23 @@ class Memory():
     max_memory_len = 1e5
 
     def remember(self, state, action, reward, done, next_state=None, info={}):
-            for key, value in zip(self.MEMORY_KEYS, (state, action, reward, next_state, done, info)):
+        for key, value in zip(self.MEMORY_KEYS, (state, action, reward, next_state, done, info)):
 
-                # Check that value is an instance of numpy.ndarray or transform the value
-                if isinstance(value, collections.Sequence):
-                    value = np.array(value)
-                elif type(value) != np.ndarray:
-                    value = np.array([value])
+            # Check that value is an instance of numpy.ndarray or transform the value
+            if isinstance(value, collections.Sequence):
+                value = np.array(value)
+            elif type(value) != np.ndarray:
+                value = np.array([value])
 
-                # Add the new experience into the memory forgetting long past experience if neccesary
-                if self.datas[key] is None:
-                    self.datas[key] = value
+            # Add the new experience into the memory forgetting long past experience if neccesary
+            if self.datas[key] is None:
+                self.datas[key] = value
+            else:
+                if len(self.datas[key]) <= self.max_memory_len:
+                    self.datas[key] = np.concatenate((self.datas[key], value))
                 else:
-                    if len(self.datas[key]) <= self.max_memory_len:
-                        self.datas[key] = np.concatenate((self.datas[key], value))
-                    else:
-                        self.datas[key] = np.roll(self.datas[key], shift=-1, axis=0)
-                        self.datas[key][-1] = np.array(value)
+                    self.datas[key] = np.roll(self.datas[key], shift=-1, axis=0)
+                    self.datas[key][-1] = np.array(value)
     
     def forget(self):
         self.datas = {key:None for key in self.MEMORY_KEYS}
