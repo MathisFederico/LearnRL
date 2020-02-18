72e4d14c843f9def85a69feb4de67502b456376c
Mathïs <mathfederico@gmail.com>
2020-02-18 13:28:42 +0100

    Correcting exploration default

--- agents/basic/agent.py
+++ agents/basic/agent.py
@@ -31,11 +31,11 @@ class BasicAgent(Agent):
     action_values = {}
     action_visits = {}
 
-    def __init__(self, evaluation=MonteCarlo(), control=UCB(), learning_rate=0.1, exploration_coef=1):
+    def __init__(self, evaluation=MonteCarlo(), control=UCB(), learning_rate=0.1, exploration=0.1):
         self.evaluation = evaluation
         self.control = control
         self.name = self.name + '_{}'.format(self.evaluation.name) + '_{}'.format(self.control.name)
-        self.exploration_coef = exploration_coef
+        self.exploration = exploration
         self.learning_rate = learning_rate
 
     def policy(self, observation, legal_actions):
@@ -43,7 +43,7 @@ class BasicAgent(Agent):
         try:
             N = np.array([self.action_visits[(state_id, action)] for action in legal_actions])
             Q = np.array([self.action_values[(state_id, action)] for action in legal_actions])
-            policy = self.control.getPolicy(action_visits=N, action_values=Q, exploration_coef=self.exploration_coef)
+            policy = self.control.getPolicy(action_visits=N, action_values=Q, exploration=self.exploration)
         except KeyError:
             policy = np.ones(legal_actions.shape)/legal_actions.shape[-1]
         
@@ -51,7 +51,7 @@ class BasicAgent(Agent):
     
     def act(self, observation, legal_actions):
         policy = self.policy(observation, legal_actions)
-        action_id = np.random.choice(legal_actions, policy) # pylint: disable=E1136  # pylint/issues/3139
+        action_id = np.random.choice(range(len(legal_actions)), p=policy) # pylint: disable=E1136  # pylint/issues/3139
         return action_id
     
     def learn(self):
