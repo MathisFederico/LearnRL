d1451ab4341df2e740e3e3de0a07cc5f3b6449ff
Mathïs <mathfederico@gmail.com>
2020-02-18 13:29:29 +0100

    Main test for frozen lake

--- main_test.py
+++ main_test.py
@@ -7,12 +7,23 @@ if __name__ == "__main__":
     env = FrozenLakeEnv()
     agent = BasicAgent()
 
-    done = False
-    state = env.reset()
+    n_games = 1000
 
-    while not done:
-        legal_actions = np.array(range(4))
-        action = agent.act(state, legal_actions)
-        next_state, reward, done , info = env.step(action)
-        agent.remember(state, action, reward, done, next_state, info)
-    agent.learn()
+    for game in range(n_games):
+        done = False
+        state = env.reset()
+        G = 0
+        while not done:
+            env.render()
+            legal_actions = np.array(range(4))
+            action = agent.act(state, legal_actions)
+            next_state, reward, done , info = env.step(action)
+            G += reward
+
+            agent.remember(state, action, reward, done, next_state, info)
+            agent.learn()
+
+            state = next_state
+        if G != 0: 
+            print('Game {}/{}, Return:{}'.format(game,n_games,G))
+            print(agent.action_values)
