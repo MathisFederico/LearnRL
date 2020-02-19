04376a9a6740e88dbd26285b54e844d755001595
Mathïs <mathfederico@gmail.com>
2020-02-19 15:25:10 +0100

    Added basic TD Removed in_dict Cleaned MC

--- agents/basic/evaluation.py
+++ agents/basic/evaluation.py
@@ -24,14 +24,7 @@ class Evaluation():
     def update_learning_rate(self):
         pass
 
-    def in_dict(self, dictionary, state, action):
-        """Check efficiently if (state, action) is in dict"""
-        keys = np.concatenate((state, action), axis=-1)
-        res = np.isin(keys, dictionary)
-        print(res)
-        return res
-
-    def learn(self, action_visits, action_values, memory, learning_rate):
+    def learn(self, action_values, memory, action_visits=None):
         raise NotImplementedError
 
 
@@ -40,30 +33,21 @@ class MonteCarlo(Evaluation):
     def __init__(self, initial_learning_rate=0.1):
         super().__init__(initial_learning_rate=initial_learning_rate, name="mc")
 
-    def learn(self, action_values, memory, learning_rate, action_visits=None):
+    def learn(self, action_values, memory, action_visits=None):
         datas = memory.datas
 
         if np.any(datas['done']):
-
             total_return = np.sum(datas['reward'])
-
-            # knowned_datas = np.map(datas['state'], datas['action'])
-
-            # Have to be optimized
-            for state_id, action in zip(datas['state'], datas['action']):
+            for state_id, action_id in zip(datas['state'], datas['action']):
                 try:
-                    # Modify the action_visits N(s, a)
-                    action_visits[(state_id, action)] += 1
-                    # Modify the action_values Q(s, a)
-                    delta = total_return - action_values[(state_id, action)]
-                    action_values[(state_id, action)] += learning_rate * delta
+                    action_visits[(state_id, action_id)] += 1
+                    delta = total_return - action_values[(state_id, action_id)]
+                    action_values[(state_id, action_id)] += self.learning_rate * delta
                 
                 # If unknown (state, action) couple
                 except KeyError:
-                    # Define the action_visits N(s, a)
-                    action_visits[(state_id, action)] = 1
-                    # Define the action_values Q(s, a)
-                    action_values[(state_id, action)] = learning_rate * total_return
+                    action_visits[(state_id, action_id)] = 1
+                    action_values[(state_id, action_id)] = self.learning_rate * total_return
             
             memory.forget()
 
@@ -71,4 +55,45 @@ class MonteCarlo(Evaluation):
 
 class TemporalDifference(Evaluation):
 
-    name = 'td'
+    def __init__(self, initial_learning_rate=0.1):
+        super().__init__(initial_learning_rate=initial_learning_rate, name="td")
+
+    def learn(self, action_values, memory, target_policy=None, online=False, action_visits=None):
+        datas = memory.datas
+
+        def get_expected_futur_reward(action_values, action, reward, done, next_state=None, target_policy=None):
+            expected_futur_reward = reward
+            if not done:
+                try:
+                    expected_futur_reward += action_values[(next_state, last_datas['action'])]
+                except KeyError:
+                    action_values[(next_state, action)] = 0
+                    action_visits[(next_state, action)] = 1
+            return expected_futur_reward
+
+
+        def td_learn(action_values, action_visits, state, action, expected_futur_reward):
+            try:
+                action_visits[(state, action)] += 1
+                delta = expected_futur_reward - action_values[(state, action)]
+                action_values[(state, action)] += self.learning_rate * delta
+            # If unknown (state, action) couple
+            except KeyError:
+                action_visits[(state, action)] = 1
+                action_values[(state, action)] = self.learning_rate * delta
+
+        # If Online learning, learns every step
+        if online:
+            last_datas = {key:datas[key][-1] for key in datas}
+            expected_futur_reward = get_expected_futur_reward(action_values, last_datas['action'],
+                                         last_datas['reward'], last_datas['done'], last_datas['next_state'], target_policy)
+            td_learn(action_values, action_visits, last_datas['state'], last_datas['action'], expected_futur_reward)
+            memory.forget()
+        
+        # If Offline learning, only learns at the end of an episode
+        else:
+            if np.any(datas['done']):
+                for state, action, reward, done, next_state in zip(datas['state'], datas['action'], datas['reward'], datas['done'], datas['next_state']):
+                    expected_futur_reward = get_expected_futur_reward(action_values, action, reward, done, next_state, target_policy)
+                    td_learn(action_values, action_visits, state, action, expected_futur_reward)
+                memory.forget()
