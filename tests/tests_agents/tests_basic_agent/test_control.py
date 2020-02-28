774ad28e771e5c99311b40e82c1b3ad41a908e8e
Mathïs <mathfederico@gmail.com>
2020-02-28 18:09:54 +0100

    Refactored to pytest Added slow making for pileline testing

--- tests/tests_agents/tests_basic_agent/test_control.py
+++ tests/tests_agents/tests_basic_agent/test_control.py
@@ -1,86 +1,82 @@
-import unittest
+import pytest
 import numpy as np
 
+class ControlError(Exception):
+    pass
 
-class ControlTest(unittest.TestCase):
+@pytest.fixture
+def action_values():
+    return np.array(range(10)[::-1])
 
-    def setUp(self):
-        self.action_values = np.array(range(10)[::-1])
-        self.action_visits = np.array(range(10)[::-1])
+@pytest.fixture
+def action_visits():
+    return np.array(range(10)[::-1])
 
-    def test_getPolicy(self):
-        from agents.basic.control import Control
-        control = Control(name="default_policy")
-        with self.assertRaises(NotImplementedError):
-            control.getPolicy(action_values=None)
+def test_getPolicy():
+    from agents.basic.control import Control
+    control = Control(name="default_policy")
+    with pytest.raises(NotImplementedError):
+        control.getPolicy(action_values=None)
 
-    def test_Greedy(self):
-        from agents.basic.control import Greedy
-        greedy = Greedy()
-        policy = greedy.getPolicy(action_values=self.action_values)
-        expected_policy = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for greedy\nExpected {}\nGot {}'.format(expected_policy, policy))
+def test_Greedy(action_values, action_visits):
+    from agents.basic.control import Greedy
 
-        greedy = Greedy(0.5)
-        policy = greedy.getPolicy(action_values=self.action_values)
-        expected_policy = np.array([0.55, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for eps-greedy with eps=0.5\nExpected {}\nGot {}'.format(expected_policy, policy))
+    expected_policies = {}
+    expected_policies[0] = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    expected_policies[0.5] = np.array([0.55, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05])
+    expected_policies[1] = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
-        greedy = Greedy(1)
-        policy = greedy.getPolicy(action_values=self.action_values)
-        expected_policy = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for eps-greedy with eps=1\nExpected {}\nGot {}'.format(expected_policy, policy))
+    for eps in [0, 0.5, 1]:
+        greedy = Greedy(eps)
+        policy = greedy.getPolicy(action_values=action_values)
+        expected_policy = expected_policies[eps]
+        if not np.all(policy==expected_policy):
+            raise ControlError(f'Wrong policy for greedy with eps={eps}\
+                                \nExpected {expected_policy}\
+                                \nGot {policy}')
 
-    def test_UCB(self):
-        from agents.basic.control import UCB
-        ucb = UCB()
+def test_UCB(action_values, action_visits):
+    from agents.basic.control import UCB
 
-        with self.assertRaises(ValueError):
-            ucb.getPolicy(action_values=None, action_visits=None)
+    ucb = UCB()
+    with pytest.raises(ValueError):
+        ucb.getPolicy(action_values=None, action_visits=None)
 
-        policy = ucb.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for UCB greedy\nExpected {}\nGot {}'.format(expected_policy, policy))
+    expected_policies = {}
+    for c in [1, 2, 10]:
+        if c < 5:
+            expected_policies[c] = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        else:
+            expected_policies[c] = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
-        ucb = UCB(2)
-        policy = ucb.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for UCB c=2\nExpected {}\nGot {}'.format(expected_policy, policy))
-        
-        ucb = UCB(10)
-        policy = ucb.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for UCB c=10\nExpected {}\nGot {}'.format(expected_policy, policy))
+    for c in [1, 2, 10]:
+        ucb = UCB(initial_exploration=c)
+        policy = ucb.getPolicy(action_values=action_values, action_visits=action_visits)
+        expected_policy = expected_policies[c]
+        if not np.all(policy==expected_policy):
+            raise ControlError(f'Wrong policy for UCB with c={c}\
+                                \nExpected {expected_policy}\
+                                \nGot {policy}')
 
-    def test_Puct(self):
-        from agents.basic.control import Puct
-        puct = Puct()
+def test_Puct(action_values, action_visits):
+    from agents.basic.control import Puct
+    puct = Puct()
 
-        with self.assertRaises(ValueError):
-            puct.getPolicy(action_values=None, action_visits=None)
+    with pytest.raises(ValueError):
+        puct.getPolicy(action_values=None, action_visits=None)
 
-        policy = puct.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for puct greedy\nExpected {}\nGot {}'.format(expected_policy, policy))
+    expected_policies = {}
+    for c in [1, 2, 10]:
+        if c < 2:
+            expected_policies[c] = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        else:
+            expected_policies[c] = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
-        puct = Puct(2)
-        policy = puct.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for puct c=2\nExpected {}\nGot {}'.format(expected_policy, policy))
-        
-        puct = Puct(10)
-        policy = puct.getPolicy(action_values=self.action_values, action_visits=self.action_visits)
-        expected_policy = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        self.assertTrue(np.all(policy==expected_policy),
-                        'Wrong policy for puct c=10\nExpected {}\nGot {}'.format(expected_policy, policy))
-
-if __name__ == "__main__":
-    unittest.main()
+    for c in [1, 2, 10]:
+        ucb = Puct(initial_exploration=c)
+        policy = ucb.getPolicy(action_values=action_values, action_visits=action_visits)
+        expected_policy = expected_policies[c]
+        if not np.all(policy==expected_policy):
+            raise ControlError(f'Wrong policy for UCB with c={c}\
+                                \nExpected {expected_policy}\
+                                \nGot {policy}')
