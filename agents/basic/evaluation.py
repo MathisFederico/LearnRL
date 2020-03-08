b89537d4c2282affd72ba944c5c70aaea4c45e44
Mathïs <mathfederico@gmail.com>
2020-03-08 19:14:43 +0100

    Changed Evaluation Added update_learning_rate with decay to default Added __str__ Added kwargs to default, MonteCarlo and TemporalDifference Refactored get_expected_reward in TD

--- agents/basic/evaluation.py
+++ agents/basic/evaluation.py
@@ -11,25 +11,31 @@ class Evaluation():
     This method must be specified : learn(self, action_values, memory, learning_rate, action_visits=None).
     """
 
-    def __init__(self, initial_learning_rate=0.1, name=None):
+    def __init__(self, initial_learning_rate=0.1, name=None, **kwargs):
         self.learning_rate = initial_learning_rate
-
+        
         if name is None:
             raise ValueError("The Control Object must have a name")
-
         self.name = name
-    
-    def update_learning_rate(self):
-        pass
+        self.decay = kwargs.get('learning_rate_decay', 1)
 
     def learn(self, action_visits, action_values, memory:Memory, **kwargs):
         raise NotImplementedError
+    
+    def update_learning_rate(self, learning_rate=None):
+        if learning_rate is not None:
+            self.learning_rate = learning_rate
+        else:
+            self.learning_rate *= self.decay
+
+    def __str__(self):
+        return self.name
 
 
 class MonteCarlo(Evaluation):
 
-    def __init__(self, initial_learning_rate=0.1):
-        super().__init__(initial_learning_rate=initial_learning_rate, name="mc")
+    def __init__(self, initial_learning_rate=0.1, **kwargs):
+        super().__init__(initial_learning_rate=initial_learning_rate, name="mc", **kwargs)
 
     def learn(self, action_visits, action_values, memory:Memory, **kwargs):
         datas = memory.datas
@@ -46,9 +52,10 @@ class MonteCarlo(Evaluation):
 
 class TemporalDifference(Evaluation):
 
-    def __init__(self, initial_learning_rate=0.1, target_policy=None):
-        super().__init__(initial_learning_rate=initial_learning_rate, name="td")
-        self.target_policy = None
+    def __init__(self, initial_learning_rate=0.1, **kwargs):
+        super().__init__(initial_learning_rate=initial_learning_rate, name="td", **kwargs)
+        self.target_policy = kwargs.get('target_policy', None)
+        self.online = kwargs.get('online', False)
 
     @staticmethod
     def get_next_legal_actions(next_state, memory):
@@ -57,10 +64,14 @@ class TemporalDifference(Evaluation):
         else:
             raise KeyError(f'Couldn\'t find state {next_state} legal_action\'s in agent memory')
 
-    def get_expected_futur_reward(self, action_values, action, reward, done, policy, next_state=None, target_policy=None):
+    def get_expected_futur_reward(self, action_visits, action_values, action, reward, done, policy, next_state=None, target_policy=None):
         expected_futur_reward = reward.astype(np.float64)
         not_done = np.logical_not(done)
-        action_impact = policy(next_state[not_done]) if target_policy is None else target_policy(next_state[not_done])
+        if target_policy is None:
+            action_impact = policy(next_state[not_done], action_values, action_visits) 
+        else:
+            action_impact = target_policy(next_state[not_done], action_values, action_visits)
+        
         expected_futur_reward[not_done] += np.sum(action_impact * action_values[next_state[not_done], :], axis=-1)
         return expected_futur_reward
     
@@ -76,17 +87,16 @@ class TemporalDifference(Evaluation):
         policy = kwargs.get('policy', None)
         if policy is None:
             raise ValueError('You must specify a policy for TD evaluation')
-        online = kwargs.get('online', True)
         datas = memory.datas
 
         # If Online learning, learns every step
         state, action, reward, done, next_state, _ = [datas[key] for key in memory.MEMORY_KEYS]
-        if online:
+        if self.online:
             if np.any(done):
                 self.learn_trajectory(action_visits, action_values, state, action, reward)
             # If not done, we estimate futur rewards based on memory and value fonctions
             else:
-                expected_futur_reward = self.get_expected_futur_reward(action_values, action, reward, done, policy,
+                expected_futur_reward = self.get_expected_futur_reward(action_visits, action_values, action, reward, done, policy,
                                                                         next_state, self.target_policy)
                 self.learn_trajectory(action_visits, action_values, state, action, expected_futur_reward)
             memory.forget()
@@ -94,7 +104,7 @@ class TemporalDifference(Evaluation):
         # If Offline learning, learns in a batch at the end of the episode
         else:
             if np.any(done):
-                expected_futur_reward = self.get_expected_futur_reward(action_values, action, reward, done,
+                expected_futur_reward = self.get_expected_futur_reward(action_visits, action_values, action, reward, done,
                                                                         policy, next_state, self.target_policy)
                 self.learn_trajectory(action_visits, action_values, state, action, expected_futur_reward)
                 memory.forget()
