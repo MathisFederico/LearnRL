a7d9f936d2263744f59d3e85bf145d8687f86a91
Mathïs <mathfederico@gmail.com>
2020-03-08 19:19:52 +0100

    Added evaluation tests and moved piplines test away Added test_name Added test_str Added test_update_learning_rate Added action_values and action_visits fixtures
    TODO : tests of classic evaluations

--- tests/tests_agents/tests_basic_agent/test_evaluation.py
+++ tests/tests_agents/tests_basic_agent/test_evaluation.py
@@ -1,139 +1,62 @@
 import pytest
 import numpy as np
-from copy import deepcopy
 
+from agents.basic.evaluation import Evaluation, MonteCarlo, TemporalDifference
+from agents.agent import Memory
+
+# Test basic evaluation features
+def test_name():
+    with pytest.raises(ValueError):
+        Evaluation()
+
+def test_str():
+    name = 'default'
+    evaluation = Evaluation(name=name)
+    if str(evaluation) != name:
+        raise ValueError(f"str(Evaluation) should return {name} and not {str(evaluation)}")
+
+def test_update_learning_rate():
+    initial_learning_rate = 0.378
+    learning_rate_decay = 0.9
+    evaluation = Evaluation(name='default', initial_learning_rate=initial_learning_rate, learning_rate_decay=learning_rate_decay)
+    if evaluation.learning_rate != initial_learning_rate:
+        raise ValueError(f"Learning rate is {evaluation.learning_rate} instead of initial_learning rate {initial_learning_rate}")
+    
+    learning_rate = 1.0
+    evaluation.update_learning_rate(learning_rate=learning_rate)
+    if evaluation.learning_rate != learning_rate:
+        raise ValueError(f"Learning rate is {evaluation.learning_rate} instead of fixed updated learning rate {learning_rate}")
+    
+    evaluation.update_learning_rate()
+    if evaluation.learning_rate != learning_rate*learning_rate_decay:
+        raise ValueError(f"Learning rate is {evaluation.learning_rate} and did not decay to {learning_rate*learning_rate_decay}")
+
+# Test learning of classic evaluations
 @pytest.fixture
 def memory():
-    from agents import Memory
     memory = Memory()
-    memory.remember(0, 0, 0, False, 1)
-    memory.remember(1, 0, -1, True, 2)
-    memory.legal_actions = {0:[0, 1], 1:[0, 1]}
     return memory
 
 @pytest.fixture
 def action_values():
-    return {(0,0):1, (0,1):0, (1, 0):1}
+    return np.array([[1, 0, -1], [-1, 1, 0]])
 
 @pytest.fixture
 def action_visits():
-    return {(0,0):1, (0,1):1, (1, 0):1}
+    return np.array([[100, 8, 3], [3, 100, 8]], dtype=np.uint16)
 
-# Monte Carlo
-@pytest.mark.slow
-def test_nim_optimal_policy():
-    from envs import NimEnv
-    from agents import BasicAgent
-    from agents.basic.evaluation import MonteCarlo
+def test_monte_carlo(action_values, action_visits, memory):
+    evaluation = MonteCarlo(initial_learning_rate=0.1)
+    raise NotImplementedError
 
-    env = NimEnv(is_optimal=True)
-    agent = BasicAgent(state_size=env.observation_space.n, action_size=env.action_space.n, evaluation=MonteCarlo())
+def test_td_0_onl_onp(action_values, action_visits, memory):
+    evaluation = TemporalDifference(initial_learning_rate=0.1, online=True)
+    raise NotImplementedError
 
-    n_games = 600
-    legal_actions = np.array(range(3))
+def test_td_0_offl_offp(action_values, action_visits, memory):
+    evaluation = TemporalDifference(initial_learning_rate=0.1, online=False, target_policy=None)
+    raise NotImplementedError
 
-    for _ in range(n_games):
-        done = False
-        state = env.reset()
-        while not done:
-            action = agent.act(state, legal_actions)
-            next_state, reward, done , info = env.step(action)
-            agent.remember(state, action, reward, done, next_state, info)
-            agent.learn()
-            state = deepcopy(next_state)
-    
-    action_size, state_size = env.action_space.n, env.observation_space.n
-    action_values = np.zeros((action_size,state_size))
-    for action in range(action_size):
-        for state in range(state_size):
-            try:
-                action_values[action, state] = agent.action_values[(state, action)]
-            except KeyError:
-                pass
-    
-    greedy_actions = 1+np.argmax(action_values, axis=0)
-    pertinent_states = np.concatenate([[2+k+4*i for k in range(3)] for i in range(4)])
-    pertinent_actions = greedy_actions[pertinent_states]
-    expected_actions = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3])
-    if not np.all(pertinent_actions==expected_actions):
-        raise ValueError(f'MonteCarlo agent got policy {pertinent_actions} instead of {expected_actions}')
-
-
-# TemporalDifference - online
-@pytest.mark.slow
-def test_td_onl_onp_nim_optimal_policy():
-    from envs import NimEnv
-    from agents import BasicAgent
-    from agents.basic.evaluation import TemporalDifference
-
-    env = NimEnv(is_optimal=True)
-    agent = BasicAgent(state_size=env.observation_space.n, action_size=env.action_space.n, evaluation=TemporalDifference())
-
-    n_games = 400
-    legal_actions = np.array(range(3))
-
-    for _ in range(n_games):
-        done = False
-        state = env.reset()
-        while not done:
-            action = agent.act(state, legal_actions)
-            next_state, reward, done , info = env.step(action)
-            agent.remember(state, action, reward, done, next_state, info)
-            agent.learn()
-            state = deepcopy(next_state)
-    
-    action_size, state_size = env.action_space.n, env.observation_space.n
-    action_values = np.zeros((action_size,state_size))
-    for action in range(action_size):
-        for state in range(state_size):
-            try:
-                action_values[action, state] = agent.action_values[(state, action)]
-            except KeyError:
-                pass
-    
-    greedy_actions = 1+np.argmax(action_values, axis=0)
-    pertinent_states = np.concatenate([[2+k+4*i for k in range(3)] for i in range(4)])
-    pertinent_actions = greedy_actions[pertinent_states]
-    expected_actions = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3])
-    if not np.all(pertinent_actions==expected_actions):
-        raise ValueError(f'TD-Onl-Onp agent got policy {pertinent_actions} instead of {expected_actions}')
-
-
-# TemporalDifference - offline
-@pytest.mark.slow
-def test_td_offl_onp_nim_optimal_policy():
-    from envs import NimEnv
-    from agents import BasicAgent
-    from agents.basic.evaluation import TemporalDifference
-
-    env = NimEnv(is_optimal=True)
-    agent = BasicAgent(state_size=env.observation_space.n, action_size=env.action_space.n, evaluation=TemporalDifference())
-
-    n_games = 400
-    legal_actions = np.array(range(3))
-
-    for _ in range(n_games):
-        done = False
-        state = env.reset()
-        while not done:
-            action = agent.act(state, legal_actions)
-            next_state, reward, done , info = env.step(action)
-            agent.remember(state, action, reward, done, next_state, info)
-            agent.learn(online=False)
-            state = deepcopy(next_state)
-    
-    action_size, state_size = env.action_space.n, env.observation_space.n
-    action_values = np.zeros((action_size,state_size))
-    for action in range(action_size):
-        for state in range(state_size):
-            try:
-                action_values[action, state] = agent.action_values[(state, action)]
-            except KeyError:
-                pass
-    
-    greedy_actions = 1+np.argmax(action_values, axis=0)
-    pertinent_states = np.concatenate([[2+k+4*i for k in range(3)] for i in range(4)])
-    pertinent_actions = greedy_actions[pertinent_states]
-    expected_actions = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3])
-    if not np.all(pertinent_actions==expected_actions):
-        raise ValueError(f'TD-Onl-Onp agent got policy {pertinent_actions} instead of {expected_actions}')
+def test_td_lamb_offl_offp(action_values, action_visits, memory):
+    evaluation = TemporalDifference(lamb=0.8, initial_learning_rate=0.1, online=False, target_policy=None)
+    raise NotImplementedError
