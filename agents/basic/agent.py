0a89d84056399ceee822a7282d10153a6de81227
Mathïs <mathfederico@gmail.com>
2020-03-08 19:03:22 +0100

    Reorganized basic_agent Changed naming conventions for basic_agent Added __str__ Added __call__ for agent action Changed default control and evaluation Added kwargs for control and evaluation

--- agents/basic/agent.py
+++ agents/basic/agent.py
@@ -21,43 +21,22 @@ class BasicAgent(Agent):
         'puct' -> puct with c=exploration
     """
 
-    name = 'BasicAgent'
-    def __init__(self, state_size, action_size,
-                       evaluation=MonteCarlo(initial_learning_rate=0.1), 
-                       control=Greedy(initial_exploration=1.0, decay=0.999)):
+    name = 'basic'
+    
+    def __init__(self, state_size, action_size, control=None, evaluation=None, **kwargs):
+        
         super().__init__()
-        self.evaluation = evaluation
-        self.control = control
-        self.name = self.name + '_{}'.format(self.evaluation.name) + '_{}'.format(self.control.name)
+        self.control = control if control is not None else Greedy(action_size, **kwargs)
+        self.evaluation = evaluation if evaluation is not None else MonteCarlo(**kwargs)
+
+        self.name = f'{self.name}_{self.control.name}_{self.evaluation.name}_{kwargs}'
+        
         self.action_values = np.zeros((state_size, action_size))
         self.action_visits = np.zeros((state_size, action_size))
-        self._hash_state = np.vectorize(self._hash_state)
-        self._hash_action = np.vectorize(self._hash_action)
-
-    @staticmethod
-    def _hash_state(state): # pylint: disable=E0202
-        if type(state) == np.ndarray:
-            state_id = hash(state.tostring())
-        else:
-            state_id = hash(state)
-        return state_id
-
-    @staticmethod
-    def _hash_action(action): # pylint: disable=E0202
-        if type(action) == np.ndarray:
-            action_id = hash(action.tostring())
-        else:
-            action_id = hash(action)
-        return action_id
-
-    def policy(self, state):
-        N = self.action_visits[state, :]
-        Q = self.action_values[state, :]
-        return self.control.getPolicy(action_visits=N, action_values=Q)
     
-    def act(self, state, legal_actions):
+    def act(self, state, legal_actions, greedy=False):
         state_id = self._hash_state(state)
-        policy = self.policy(state_id)
+        policy = self.control.get_policy(state_id, self.action_values, self.action_visits)
 
         legal_actions_id = np.array([self._hash_action(action) for action in legal_actions])
         legal_policy = policy[legal_actions_id]
@@ -69,6 +48,28 @@ class BasicAgent(Agent):
         self.memory.remember(self._hash_state(state), self._hash_action(action), reward, done, self._hash_state(next_state), info)
 
     def learn(self, **kwargs):
-        self.evaluation.learn(action_values=self.action_values, action_visits=self.action_visits, memory=self.memory, policy=self.policy, **kwargs)
-        self.control.updateExploration()
+        self.evaluation.learn(action_values=self.action_values, action_visits=self.action_visits, memory=self.memory, policy=self.control.get_policy, **kwargs)
+        self.control.update_exploration()
         self.evaluation.update_learning_rate()
+
+    def __str__(self):
+        return self.name
+    
+    def __call__(self, state, legal_actions, greedy=False):
+        return self.act(state, legal_actions, greedy=False)
+
+    @staticmethod
+    def _hash_state(state):
+        if type(state) == np.ndarray:
+            state_id = hash(state.tostring())
+        else:
+            state_id = hash(state)
+        return state_id
+
+    @staticmethod
+    def _hash_action(action):
+        if type(action) == np.ndarray:
+            action_id = hash(action.tostring())
+        else:
+            action_id = hash(action)
+        return action_id
