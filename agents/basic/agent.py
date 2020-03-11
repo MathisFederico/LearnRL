903f6e5739922a4a8ec09e44422e3bc4a5da8983
Mathïs Fédérico <60117466+MathisFederico@users.noreply.github.com>
2020-03-12 00:56:48 +0100

    Feature/ndarray table (#29)
    * Changed agent desc
    
    * Fixed bug for 1D ndarrays
    Refactored value checking
    
    * Refactored action_values/action_visits (dict->ndarray)
    Implemented batch methods for MonteCarlo, Temporal Difference
    
    * Added test_basic_agent
    Removed test_agents
    Added test_name
    
    TODO : test_hash and test_act
    
    * Added Greedy to main test
    
    * Reorganized basic_agent
    Changed naming conventions for basic_agent
    Added __str__
    Added __call__ for agent action
    Changed default control and evaluation
    Added kwargs for control and evaluation
    
    * Refactored Control
    Renamed getPolicy to get_policy
    Added check_policy in get_policy
    Added policy the simple policy for one state
    Added apply_along axis(policy) in get_policy
    Added update_exploration with decay in default control
    Added __str__
    Refactored Greedy UCB and Puct
    
    * Changed Evaluation
    Added update_learning_rate with decay to default
    Added __str__
    Added kwargs to default, MonteCarlo and TemporalDifference
    Refactored get_expected_reward in TD
    
    * Change Nim random to numpy for seeding
    
    * Added new tests in test_control
    Added test_name
    Added test_str
    Added test_update_exploration
    Changed action_values and action_visits fixtures
    Refactored test_Greedy
    Added 2D test_Greedy
    Refactored test_UCB
    Refactored test_Puct
    
    * Added evaluation tests and moved piplines test away
    Added test_name
    Added test_str
    Added test_update_learning_rate
    Added action_values and action_visits fixtures
    
    TODO : tests of classic evaluations

--- agents/basic/agent.py
+++ agents/basic/agent.py
@@ -12,67 +12,64 @@ class BasicAgent(Agent):
     
     Evaluations : agents.basic.evaluation
         'mc','montecarlo' -> Monte-Carlo evaluation
-        X'td','temp*dif*' -> Offline Temporal Difference
-        X'ontd','on*temp*dif' -> Online Temporal Difference
         X'sarsa' -> SARSA with specified target policy
         X'q*' -> QLearning (SARSA with greedy target policy)
         
     Control : agents.basic.control
-        '*greedy' -> epsilon_greedy with epsilon=exploration
+        'greedy' -> epsilon_greedy with epsilon=exploration
         'ucb' -> ucb with c=exploration
         'puct' -> puct with c=exploration
     """
 
-    name = 'BasicAgent'
-
-    action_values = {}
-    action_visits = {}
-
-    def __init__(self, evaluation=MonteCarlo(initial_learning_rate=0.1), control=Greedy(initial_exploration=1.0, decay=0.999)):
-        super().__init__()
-        self.evaluation = evaluation
-        self.control = control
-        self.name = self.name + '_{}'.format(self.evaluation.name) + '_{}'.format(self.control.name)
-        self.memory.legal_actions = {}
-
-    @staticmethod
-    def _hash_state(state):
-        try:
-            state_id = hash(state)
-        except TypeError:
-            state_id = hash(state.tostring())
-        return state_id
-
-    @staticmethod
-    def _hash_action(action):
-        try:
-            action_id = hash(action)
-        except TypeError:
-            action_id = hash(action.tostring())
-        return action_id
-
-    def policy(self, state, legal_actions):     
-        try:
-            N = np.array([self.action_visits[(state, action)] for action in legal_actions])
-            Q = np.array([self.action_values[(state, action)] for action in legal_actions])
-            policy = self.control.getPolicy(action_visits=N, action_values=Q)
-        except KeyError:
-            policy = np.ones(legal_actions.shape)/legal_actions.shape[-1]
-        return policy
+    name = 'basic'
     
-    def act(self, state, legal_actions):
-        state_id = self._hash_state(state)
-        legal_actions_id = np.array([self._hash_action(action) for action in legal_actions])
-        self.memory.legal_actions[state_id] = legal_actions_id
+    def __init__(self, state_size, action_size, control=None, evaluation=None, **kwargs):
+        
+        super().__init__()
+        self.control = control if control is not None else Greedy(action_size, **kwargs)
+        self.evaluation = evaluation if evaluation is not None else MonteCarlo(**kwargs)
 
-        policy = self.policy(state_id, legal_actions_id)
-        action_id = np.random.choice(range(len(legal_actions)), p=policy) # pylint: disable=E1136  # pylint/issues/3139
+        self.name = f'{self.name}_{self.control.name}_{self.evaluation.name}_{kwargs}'
+        
+        self.action_values = np.zeros((state_size, action_size))
+        self.action_visits = np.zeros((state_size, action_size))
+    
+    def act(self, state, legal_actions, greedy=False):
+        state_id = self._hash_state(state)
+        policy = self.control.get_policy(state_id, self.action_values, self.action_visits)
+
+        legal_actions_id = np.array([self._hash_action(action) for action in legal_actions])
+        legal_policy = policy[legal_actions_id]
+
+        action_id = np.random.choice(range(len(legal_actions)), p=legal_policy) # pylint: disable=E1136  # pylint/issues/3139
         return action_id
     
     def remember(self, state, action, reward, done, next_state=None, info={}):
         self.memory.remember(self._hash_state(state), self._hash_action(action), reward, done, self._hash_state(next_state), info)
 
     def learn(self, **kwargs):
-        self.evaluation.learn(action_values=self.action_values, action_visits=self.action_visits, memory=self.memory, policy=self.policy, **kwargs)
-        self.control.updateExploration()
+        self.evaluation.learn(action_values=self.action_values, action_visits=self.action_visits, memory=self.memory, policy=self.control.get_policy, **kwargs)
+        self.control.update_exploration()
         self.evaluation.update_learning_rate()
+
+    def __str__(self):
+        return self.name
+    
+    def __call__(self, state, legal_actions, greedy=False):
+        return self.act(state, legal_actions, greedy=False)
+
+    @staticmethod
+    def _hash_state(state):
+        if type(state) == np.ndarray:
+            state_id = hash(state.tostring())
+        else:
+            state_id = hash(state)
+        return state_id
+
+    @staticmethod
+    def _hash_action(action):
+        if type(action) == np.ndarray:
+            action_id = hash(action.tostring())
+        else:
+            action_id = hash(action)
+        return action_id
--- agents/basic/evaluation.py
+++ agents/basic/evaluation.py
@@ -11,50 +11,51 @@ class Evaluation():
     This method must be specified : learn(self, action_values, memory, learning_rate, action_visits=None).
     """
 
-    def __init__(self, initial_learning_rate=0.1, name=None):
+    def __init__(self, initial_learning_rate=0.1, name=None, **kwargs):
         self.learning_rate = initial_learning_rate
-
+        
         if name is None:
             raise ValueError("The Control Object must have a name")
-
         self.name = name
-    
-    def update_learning_rate(self):
-        pass
+        self.decay = kwargs.get('learning_rate_decay', 1)
 
     def learn(self, action_visits, action_values, memory:Memory, **kwargs):
         raise NotImplementedError
+    
+    def update_learning_rate(self, learning_rate=None):
+        if learning_rate is not None:
+            self.learning_rate = learning_rate
+        else:
+            self.learning_rate *= self.decay
+
+    def __str__(self):
+        return self.name
 
 
 class MonteCarlo(Evaluation):
 
-    def __init__(self, initial_learning_rate=0.1):
-        super().__init__(initial_learning_rate=initial_learning_rate, name="mc")
+    def __init__(self, initial_learning_rate=0.1, **kwargs):
+        super().__init__(initial_learning_rate=initial_learning_rate, name="mc", **kwargs)
 
     def learn(self, action_visits, action_values, memory:Memory, **kwargs):
         datas = memory.datas
 
         if np.any(datas['done']):
             total_return = np.sum(datas['reward'])
-            for state, action in zip(datas['state'], datas['action']):
-                try:
-                    action_visits[(state, action)] += 1
-                    delta = total_return - action_values[(state, action)]
-                    action_values[(state, action)] += self.learning_rate * delta
-                
-                # If unknown (state, action) couple
-                except KeyError:
-                    action_visits[(state, action)] = 1
-                    action_values[(state, action)] = self.learning_rate * total_return
-            
+
+            action_visits[datas['state'], datas['action']] += 1
+            delta = total_return - action_values[datas['state'], datas['action']]
+            action_values[datas['state'], datas['action']] += self.learning_rate * delta
+
             memory.forget()
 
 
 class TemporalDifference(Evaluation):
 
-    def __init__(self, initial_learning_rate=0.1, target_policy=None):
-        super().__init__(initial_learning_rate=initial_learning_rate, name="td")
-        self.target_policy = None
+    def __init__(self, initial_learning_rate=0.1, **kwargs):
+        super().__init__(initial_learning_rate=initial_learning_rate, name="td", **kwargs)
+        self.target_policy = kwargs.get('target_policy', None)
+        self.online = kwargs.get('online', False)
 
     @staticmethod
     def get_next_legal_actions(next_state, memory):
@@ -63,30 +64,21 @@ class TemporalDifference(Evaluation):
         else:
             raise KeyError(f'Couldn\'t find state {next_state} legal_action\'s in agent memory')
 
-    def get_expected_futur_reward(self, action_values, action, reward, done, policy, next_state=None, next_legal_actions=None, target_policy=None):
-        expected_futur_reward = reward
-        if not done:
-            for next_action in next_legal_actions:
-                # On vs Off Policy
-                if target_policy is not None:
-                    action_impact = target_policy(next_state, next_legal_actions)[next_action]
-                else:
-                    action_impact = policy(next_state, next_legal_actions)[next_action]
-                # Only add expected reward from actions we took at least once
-                if (next_state, next_action) in action_values:
-                    expected_futur_reward += action_impact*action_values[(next_state, next_action)]
-       
+    def get_expected_futur_reward(self, action_visits, action_values, action, reward, done, policy, next_state=None, target_policy=None):
+        expected_futur_reward = reward.astype(np.float64)
+        not_done = np.logical_not(done)
+        if target_policy is None:
+            action_impact = policy(next_state[not_done], action_values, action_visits) 
+        else:
+            action_impact = target_policy(next_state[not_done], action_values, action_visits)
+        
+        expected_futur_reward[not_done] += np.sum(action_impact * action_values[next_state[not_done], :], axis=-1)
         return expected_futur_reward
     
     def learn_trajectory(self, action_visits, action_values, state, action, expected_futur_reward):
-        try:
-            action_visits[(state, action)] += 1
-            delta = expected_futur_reward - action_values[(state, action)]
-            action_values[(state, action)] += self.learning_rate * delta
-        # If unknown (state, action) couple
-        except KeyError:
-            action_visits[(state, action)] = 1
-            action_values[(state, action)] = self.learning_rate * expected_futur_reward
+        action_visits[state, action] += 1
+        delta = expected_futur_reward - action_values[state, action]
+        action_values[state, action] += self.learning_rate * delta
 
 
     def learn(self, action_visits, action_values, memory:Memory, **kwargs):
@@ -95,28 +87,24 @@ class TemporalDifference(Evaluation):
         policy = kwargs.get('policy', None)
         if policy is None:
             raise ValueError('You must specify a policy for TD evaluation')
-        online = kwargs.get('online', True)
         datas = memory.datas
 
         # If Online learning, learns every step
-        if online:
-            state, action, reward, done, next_state, _ = [datas[key][-1] for key in memory.MEMORY_KEYS]
-            if done:
+        state, action, reward, done, next_state, _ = [datas[key] for key in memory.MEMORY_KEYS]
+        if self.online:
+            if np.any(done):
                 self.learn_trajectory(action_visits, action_values, state, action, reward)
             # If not done, we estimate futur rewards based on memory and value fonctions
             else:
-                # Only learn if we know what is next_state
-                if next_state in memory.legal_actions:
-                    next_legal_actions = self.get_next_legal_actions(next_state, memory)
-                    expected_futur_reward = self.get_expected_futur_reward(action_values, action, reward, done, policy,
-                                                                        next_state, next_legal_actions, self.target_policy)
-                    self.learn_trajectory(action_visits, action_values, state, action, expected_futur_reward)
+                expected_futur_reward = self.get_expected_futur_reward(action_visits, action_values, action, reward, done, policy,
+                                                                        next_state, self.target_policy)
+                self.learn_trajectory(action_visits, action_values, state, action, expected_futur_reward)
             memory.forget()
         
         # If Offline learning, learns in a batch at the end of the episode
-        # else:
-        #     if np.any(datas['done']):
-        #         for state, action, reward, done, next_state in zip(datas['state'], datas['action'], datas['reward'], datas['done'], datas['next_state']):
-        #             expected_futur_reward = self.get_expected_futur_reward(action_values, action, reward, done, policy, next_state, self.target_policy)
-        #             self.learn_trajectory(action_values, action_visits, state, action, expected_futur_reward)
-        #         memory.forget()
+        else:
+            if np.any(done):
+                expected_futur_reward = self.get_expected_futur_reward(action_visits, action_values, action, reward, done,
+                                                                        policy, next_state, self.target_policy)
+                self.learn_trajectory(action_visits, action_values, state, action, expected_futur_reward)
+                memory.forget()
