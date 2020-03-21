58828b971895e269ac7c6f7a07e32e86173ec252
Mathïs Fédérico <mathfederico@gmail.com>
2020-03-21 04:40:11 +0100

    Fixed CrossesAndNoughts for agent with no legal_actions

--- core_test.py
+++ core_test.py
@@ -1,14 +1,16 @@
 from core import Playground
-from envs import NimEnv
+from envs import NimEnv, CrossesAndNoughtsEnv
 from agents import BasicAgent
 from agents.basic.control import Greedy
 from agents.basic.evaluation import TemporalDifference
+import numpy as np
 
-env = NimEnv(is_optimal=True)
+env = CrossesAndNoughtsEnv()
+action_size = np.prod(env.action_space.nvec)
 agent = BasicAgent(state_space=env.observation_space, action_space=env.action_space,
-                   control=Greedy(env.action_space.n, initial_exploration=0.1, exploration_decay=1-5e-5),
+                   control=Greedy(action_size, initial_exploration=0.1, exploration_decay=1-5e-5),
                    evaluation=TemporalDifference(initial_learning_rate=0.2, online=True))
 
 pg = Playground(env, agent)
-pg.fit(1000, verbose=2)
+pg.fit(20000, verbose=2)
 pg.run(10)
--- envs/single_agents_envs/random_crosses_and_noughts/game.py
+++ envs/single_agents_envs/random_crosses_and_noughts/game.py
@@ -255,15 +255,18 @@ class CrossesAndNoughtsEnv(Env):
             else:
                 # Need play random instead
                 legal_actions = self.game.getLegalActions()
-                rd_action = np.random.choice(legal_actions)
-                self.game.play(player, rd_action)
-                print("Invalid action, played at random, reward is -1")
-                pass_turn = True
+                if len(legal_actions) > 0:
+                    rd_action = legal_actions[np.random.choice(range(len(legal_actions)))]
+                    self.game.play(other_player, rd_action)
+                    # print("Invalid action, played at random, reward is -1")
+                    pass_turn = True
+                    reward, done = checkStep(player, other_player)
+                    return observation, -1, done, {'pass_turn':pass_turn}
         
         reward, done = checkStep(player, other_player)
         
         if self.vs_random:
-            if not done:
+            if not done and not pass_turn:
                 legal_actions = self.game.getLegalActions()
                 if len(legal_actions) > 0:
                     rd_action = legal_actions[np.random.choice(range(len(legal_actions)))]
