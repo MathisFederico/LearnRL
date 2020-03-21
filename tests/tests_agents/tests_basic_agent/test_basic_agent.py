de3dda065de056228c295d08b1ac6398bf4f12f4
Mathïs Fédérico <mathfederico@gmail.com>
2020-03-21 20:18:13 +0100

    Corrected test_basic_agent with new name

--- tests/tests_agents/tests_basic_agent/test_basic_agent.py
+++ tests/tests_agents/tests_basic_agent/test_basic_agent.py
@@ -13,14 +13,14 @@ def agent():
     return TableAgent(Discrete(3), Discrete(4))
 
 def test_name(agent):
-    default_name = r"basic_greedy_qlearning_{}"
+    default_name = r"table_greedy_qlearning_{}"
     if agent.name != default_name:
         raise ValueError(f'Default name should be {default_name} and not {agent.name}')
     if str(agent) != default_name:
         raise ValueError(f'str(BasicAgent) should be {default_name}(agent name) and not {str(agent)}')
     
     agent = TableAgent(Discrete(3), Discrete(4), foo='foo')
-    default_name = r"basic_greedy_qlearning_{'foo': 'foo'}"
+    default_name = r"table_greedy_qlearning_{'foo': 'foo'}"
     if agent.name != default_name:
         raise ValueError(f'Default name should be {default_name} and not {agent.name}')
 
