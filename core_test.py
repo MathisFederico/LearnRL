5ea2f75f31683e9a2edc3ab73ec0d0fa2c8dd860
Mathïs Fédérico <mathfederico@gmail.com>
2020-03-21 03:46:36 +0100

    Added core_test with success

--- core_test.py
+++ core_test.py
@@ -1,9 +1,14 @@
 from core import Playground
 from envs import NimEnv
 from agents import BasicAgent
+from agents.basic.control import Greedy
+from agents.basic.evaluation import TemporalDifference
 
-env = NimEnv()
-agent = BasicAgent(env.observation_space.n, env.action_space.n)
+env = NimEnv(is_optimal=True)
+agent = BasicAgent(state_space=env.observation_space, action_space=env.action_space,
+                   control=Greedy(env.action_space.n, initial_exploration=0.1, exploration_decay=1-5e-5),
+                   evaluation=TemporalDifference(initial_learning_rate=0.2, online=True))
+
 pg = Playground(env, agent)
-
-pg.fit(1000, verbose=1)
+pg.fit(1000, verbose=2)
+pg.run(10)
