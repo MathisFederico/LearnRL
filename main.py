ae838e38a335d27255b6dfe2a5bdfc5991a65663
Mathïs Fédérico <mathfederico@gmail.com>
2020-03-22 15:09:43 +0100

    Adding documentation

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 # Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
 import learnrl as rl
-from learnrl.environments import RdNimEnv, CrossesAndNoughtsEnv
+from learnrl.environments import CrossesAndNoughtsEnv
 from learnrl.agents import TableAgent
-from learnrl.agents.table import Greedy, TemporalDifference
-import numpy as np
 
 env = CrossesAndNoughtsEnv()
 agent1 = TableAgent(state_space=env.observation_space, action_space=env.action_space)
