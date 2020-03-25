d49aa1314533fd459f4f049e08f2258e46e6ceaa
Mathïs Fédérico <mathfederico@gmail.com>
2020-03-25 16:38:50 +0100

    Fixed versionning

--- learnrl/__init__.py
+++ learnrl/__init__.py
@@ -1,10 +1,7 @@
 # LearnRL a python library to learn and use reinforcement learning
 # Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-def get_version():
-    version_file = open('../VERSION')
-    return version_file.read().strip()
-__version__ = get_version()
+__version__ = "0.1.1.2"
 
 from learnrl.core import *
 import learnrl.environments
