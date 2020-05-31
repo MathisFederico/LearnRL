c7d3e117405fc4f54691073e244a6b84894b075d
Mathïs Fédérico <mathfederico@gmail.com>
2020-05-31 22:46:02 +0200

    Added Memory.sample Minor fixes in Memory

--- learnrl/core.py
+++ learnrl/core.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import collections.abc as collections
+from copy import copy
 
 from time import time
 from gym import Env
@@ -55,7 +56,7 @@ class Memory():
         
         """
 
-        def remember_key(datas, key, value, max_memory_len=self.max_memory_len):
+        def _remember_key(datas, key, value, max_memory_len=self.max_memory_len):
             if datas[key] is None:
                 datas[key] = value
             else:
@@ -71,11 +72,49 @@ class Memory():
                 value = value[np.newaxis, ...]
             if isinstance(value, collections.Sequence) or type(value) != np.ndarray:
                 value = np.array([value])
-            remember_key(self.datas, key, value)
+            _remember_key(self.datas, key, value)
         
         # Add optional suplementary parameters
         for key in param:
-            remember_key(self.datas, key, param[key])
+            _remember_key(self.datas, key, param[key])
+
+
+    def sample(self, sample_size=0, method='naive_uniform', return_copy=True):
+        """ Return a sample of experiences stored in the memory
+        
+        Parameters
+        ----------
+            sample_size: int
+                The size of the sample to get from memory, if 0 return all memory.
+            method: str
+                On of ("last", "naive_uniform", "uniform"). The sampling method.
+            copy: bool
+                If True, return a copy of the memory sampled.
+        
+        Return
+        ------
+            datas: list
+                The list of :class:`numpy.ndarray` of memory samples for each key in MEMORY_KEYS.
+        
+        """
+        if method not in ['naive_uniform', 'last']:
+            raise NotImplementedError(f'Method {method} is not implemented yet')
+        
+        n_experiences = len(self.datas['observation'])
+
+        if n_experiences <= sample_size or sample_size == 0:
+            datas = [self.datas[key] for key in self.MEMORY_KEYS]
+        else:
+            if method == 'naive_uniform':
+                sample_indexes = np.random.choice(np.arange(n_experiences), size=sample_size)
+            elif method == 'last':
+                sample_indexes = np.arange(n_experiences - sample_size, n_experiences)
+            datas = [self.datas[key][sample_indexes] for key in self.MEMORY_KEYS]
+
+        if return_copy:
+            datas = [copy(value) for value in datas]
+
+        return datas
 
     def forget(self):
         """ Remove all memory"""
@@ -91,7 +130,6 @@ class Agent():
     """
 
     name = None
-    memory = Memory()
 
     def act(self, observation):
         """ How the agent act given an observation
@@ -121,14 +159,8 @@ class Agent():
             ...                       self.observation_encoder(next_observation), 
             ...                       info, **param)
         """
-        # self.memory.remember(self._hash_observation(observation), self._hash_action(action), reward, done, self._hash_observation(next_observation), info, **param)
         raise NotImplementedError
     
-    def forget(self):
-        """ Make the agent forget his memory """
-        self.memory.forget()
-    
-
 
 class MultiEnv(Env):
 
