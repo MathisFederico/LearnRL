3ac97342a95361400f49d5ba61d7c44ae1aa464b
Mathïs Fédérico <mathfederico@gmail.com>
2020-06-12 12:49:25 +0200

    Remove slow mark on numpy tests

--- tests/tests_agents/tests_standard/test_numpy.py
+++ tests/tests_agents/tests_standard/test_numpy.py
@@ -30,7 +30,6 @@ def pertinent_observations(n_triplets):
 def expected_optimal_actions(n_triplets):
     return np.concatenate([(0, 1, 2) for _ in range(n_triplets-1)])
 
-@pytest.mark.slow
 def test_evalutations(env, pertinent_observations, expected_optimal_actions):
     evaluations = [
         (QLearning(learning_rate=0.3), True),
@@ -56,8 +55,6 @@ def test_evalutations(env, pertinent_observations, expected_optimal_actions):
         if not np.all(pertinent_actions==expected_optimal_actions):
             raise ValueError(f'Got policy {pertinent_actions} instead of {expected_optimal_actions} for evaluation {str(evaluation)}')
 
-
-@pytest.mark.slow
 def test_controls(env, pertinent_observations, expected_optimal_actions):
     controls = [
         Greedy(exploration=0.1),
