1c5afb18ba454b4a0f0e3c137f0933053059bc03
MathisFederico <mathfederico@gmail.com>
2020-06-25 20:17:21 +0200

    Updated logging

--- learnrl/callbacks.py
+++ learnrl/callbacks.py
@@ -103,76 +103,167 @@ class CallbackList():
     def on_run_end(self, logs=None):
         self._call_key_hook('run', 'end', logs=logs)
 
+
+class Metric():
+
+    def __init__(self, metric_code):
+        split_code = metric_code.split('.')
+        self.code = metric_code
+        fullname = split_code[0]
+        split_name = fullname.split('~')
+        self.name = split_name[0]
+        self.surname = split_name[1] if len(split_name) > 1 else self.name
+        self.operator = split_code[1] if len(split_code) > 1 else 'avg'
+    
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        return self.name == other.name
+    
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.code
+
+class MetricList():
+
+    def __init__(self, metric_list):
+        self.metric_list = [self.to_metric(m) for m in metric_list]
+        self.metric_names = [m.name for m in self.metric_list]
+    
+    def __getitem__(self, index):
+        return self.metric_list[index]
+    
+    def __contains__(self, metric:Metric):
+        return metric.name in self.metric_names
+
+    def __add__(self, metric_list):
+        new_list = self.metric_list + [self.to_metric(m) for m in metric_list if m not in self]
+        return MetricList(new_list)
+
+    def append(self, metric:Metric):
+        if metric not in self:
+            self.metric_list += [self.to_metric(metric)]
+            self.metric_names += [metric.name]
+        else:
+            raise ValueError(f'{metric} already in MetricList')
+    
+    def __str__(self):
+        return str(self.metric_list)
+    
+    @staticmethod
+    def to_metric(metric):
+        if isinstance(metric, Metric):
+            return metric
+        else:
+            return Metric(metric)
+
+
 class Logger(Callback):
 
     """ Default logger in every :class:`~learnrl.core.Playground` run """
 
-    bar_lenght = 40
+    def __init__(self, detailed_step_only_metrics=['observation', 'action', 'next_observation'],
+                       step_only_metrics=['done'],
+                       step_metrics=['reward', 'loss', 'exploration~exp', 'learning_rate~lr', 'dt_step~'],
+                       episode_only_metrics=[], 
+                       episode_metrics=['reward.sum', 'loss', 'exploration~exp.last', 'learning_rate~lr.last', 'dt_episode~', 'dt_step~'],
+                       cycle_metrics=['reward~rwd', 'loss', 'exploration~exp.last', 'learning_rate~lr.last', 'dt_episode~', 'dt_step~'],
+                       cycle_only_metrics=[]):
+
+        self.cycle_only_metrics = MetricList(cycle_only_metrics)
+        self.cycle_metrics = MetricList(cycle_metrics)
+
+        self.episode_only_metrics = MetricList(episode_only_metrics)
+        self.episode_metrics = MetricList(episode_metrics)
+
+        self.step_only_metrics = MetricList(step_only_metrics)
+        self.step_metrics = MetricList(step_metrics)
+
+        self.detailed_step_only_metrics = MetricList(detailed_step_only_metrics)
+
+        self._bar_lenght = 50    
 
     def on_step_begin(self, step, logs=None):
+        text = f'Step {step+1}'
         if self.params['verbose'] == 3:
-            print(f'Step {step+1}', end='\t| ')
+            print(text, end='\t| ')
         elif self.params['verbose'] > 3:
-            text = f'Step {step+1}'
-            semibar_lenght = (self.bar_lenght - len(text)) // 2 - 1
-            odd = (self.bar_lenght - len(text)) % 2 == 1
-            print('-' * semibar_lenght + f' {text} ' + '-' * (semibar_lenght + 1*odd))
+            self._print_bar(text, '-')
 
-    def on_step_end(self, step, logs=None):
-        self.episode_seen_steps += 1
-        self.cycle_seen_steps += 1
-
+    def on_step_end(self, step, logs={}):
         agent_id = logs.get('agent_id')
-        reward = logs.get('reward')
-        done = logs.get('done')
-        self.returns[agent_id] += reward
+        verbose = self.params['verbose']
 
-        step_time = logs.get('dt_step')
-        self.episode_step_time += (step_time - self.episode_step_time) / self.episode_seen_steps
-        self.cycle_step_time += (step_time - self.cycle_step_time) / self.cycle_seen_steps
+        if self.n_agents > 1 and verbose > 2:
+            print(f"Agent :{logs.get('agent_id')}", end='\t| ')
+
+        if verbose > 2:
+            for metric in self.step_only_metrics + self.step_metrics:
+                metric_value = self._extract_metric_from_logs(metric.name, logs, agent_id)
+                if metric_value != 'N/A':
+                    end = '\t| ' if verbose == 3 else None
+                    self._print_metric(metric, metric_value, end=end)                    
+            if verbose == 3: print()
+
+        if verbose > 3:
+            for metric in self.detailed_step_only_metrics:
+                metric_value = self._extract_metric_from_logs(metric.name, logs, agent_id)
+                if metric_value != 'N/A':
+                    self._print_metric(metric, metric_value)
+            print('-'*self._bar_lenght, end='\n\n')
         
-        if self.params['verbose'] == 3:
-            if self.n_agents > 1:
-                print(f"Agent :{logs.get('agent_id')}", end='\t| ')
-            print(f"Reward {reward}", end='\t| ')
-            print(self._get_time_text(step_time, 'step'))
-            if done:
-                print()
-        elif self.params['verbose'] > 3:
-            if self.n_agents > 1:
-                print(f"Agent {agent_id}")
-            print(f"Observation {logs.get('observation')}")
-            print(f"Action {logs.get('action')}")
-            print(f"Reward {reward}")
-            print(f"Done {done}")
-            print(f"Next Observation {logs.get('next_observation')}")
-            print(self._get_time_text(step_time, 'step'))
-            print('-'*self.bar_lenght, end='\n\n')
+        if verbose in (1, 2):
+            for metric in self.episode_metrics:
+                if not metric == 'dt_episode':
+                    metric_value = self._extract_metric_from_logs(metric.name, logs, agent_id)
+                    if metric_value != 'N/A':
+                        attr_name = 'episode_' + metric.name
+                        self._update_attr(attr_name, metric_value, metric.operator)
+
 
     def on_episode_begin(self, episode, logs=None):
-        self.returns = np.zeros(self.n_agents)
-        self.episode_step_time = 0
-        self.episode_seen_steps = 0
-        if self.params['verbose'] > 2:
-            print("="*self.bar_lenght)
+        for metric in self.episode_metrics:
+            attrname = 'episode_' + metric.name
+            setattr(self, attrname, 0)
+
         if self.params['verbose'] >= 2:
-            print("Episode " + self._get_episode_text(episode), end=' | ')
-        if self.params['verbose'] > 2:
-            print()
+            text = "Episode " + self._get_episode_text(episode)
+            if self.params['verbose'] == 2:
+                print(text, end=' | ')
+            else:
+                self._print_bar(text, '=')
 
     def on_episode_end(self, episode, logs=None):
-        self.cycle_seen_episodes += 1
-        self.avg_returns += (self.returns - self.avg_returns) / self.cycle_seen_episodes
+        verbose = self.params['verbose']
+        if verbose >= 2:
+            if verbose == 3:
+                print()
 
-        episode_time = logs.get('dt_episode')
-        self.cycle_episode_time += (episode_time - self.cycle_episode_time) / self.cycle_seen_episodes
+            for metric in self.episode_only_metrics:
+                metric_value = self._extract_metric_from_logs(metric, logs)
+                if metric_value != 'N/A':
+                    self._print_metric(metric, metric_value, end='\t| ')
+            
+            for metric in self.episode_metrics:
+                episode_name = 'episode_' + metric.name
+                episode_value = getattr(self, episode_name, 'N/A') if not metric == 'dt_episode' else logs.get(metric.name)
+                if episode_value != 'N/A':
+                     self._print_metric(metric, episode_value,  end='\t| ')
 
-        if self.params['verbose'] >= 2:
-            print(f"Returns {self.returns}", end='\t| ')
-            print(self._get_time_text(episode_time, 'episode'), end='\t| ')
-            print(self._get_time_text(self.episode_step_time, 'step'))
-        if self.params['verbose'] > 2:
-            print("="*self.bar_lenght, end='\n\n')
+            print()
+
+        if verbose > 2:
+            print("="*self._bar_lenght, end='\n\n')
+        
+        if verbose == 1:
+            for metric in self.cycle_metrics:
+                episode_name = 'episode_' + metric.name
+                episode_value = getattr(self, episode_name, 'N/A') if not metric == 'dt_episode' else logs.get(metric.name)
+                
+                cycle_name = 'cycle_' + metric.name
+                self._update_attr(cycle_name, episode_value, metric.operator)
 
     def on_cycle_begin(self, episode, logs=None):
         self.avg_returns = np.zeros(self.n_agents)
@@ -183,21 +274,67 @@ class Logger(Callback):
         self.cycle_seen_steps = 0
         self.cycle_step_time = 0
 
+        self.cycle_loss = 0
+
     def on_cycle_end(self, episode, logs=None):
-        if self.params['verbose'] == 1:
+         if self.params['verbose'] == 1:
             print("Episode " + self._get_episode_text(episode), end=' | ')
-            print(f"Returns {self.avg_returns}", end='\t| ')
-            print(self._get_time_text(self.cycle_episode_time, 'episode'), end='\t| ')
-            print(self._get_time_text(self.cycle_step_time, 'step'))
+            
+            for metric in self.cycle_only_metrics:
+                metric_value = self._extract_metric_from_logs(metric, logs)
+                if metric_value != 'N/A':
+                    self._print_metric(metric, metric_value, end='\t| ')
+            
+            for metric in self.cycle_metrics:
+                cycle_name = 'cycle_' + metric.name
+                cycle_value = getattr(self, cycle_name, 'N/A')
+                if cycle_value != 'N/A':
+                     self._print_metric(metric, cycle_value,  end='\t| ')
+            
+            print()
 
     def on_run_begin(self, logs=None):
         self.n_agents = len(self.playground.agents)
         self.n_digits_episodes = int(np.log10(self.params['episodes'])) + 1
         if self.params['verbose'] >= 1:
-            print('Run started')
+            print('***** Run started *****')
 
     def on_run_end(self, logs=None):
         pass
+
+    def _print_metric(self, metric, metric_value, **kwargs):
+        if metric.name.startswith('dt_'):
+            level = metric.name.split('_')[1]
+            metric_display = self._get_time_text(metric_value, level)
+        elif isinstance(metric_value, (float, np.float32, np.float64)):
+            if abs(metric_value) < 100 and abs(metric_value) > 0.01:
+                metric_display = f"{metric_value:.2f}"
+            else:
+                metric_display = f"{metric_value:.2E}"
+        else:
+            metric_display = str(metric_value)
+        space = ' ' if len(metric.surname) > 0 else ''
+        print(f"{metric.surname.capitalize()}" + space + metric_display, **kwargs)
+    
+    def _print_bar(self, text, line, **kwargs):
+        semibar_lenght = (self._bar_lenght - len(text)) // 2 - 1
+        odd = (self._bar_lenght - len(text)) % 2 == 1
+        print(line * semibar_lenght + f' {text} ' + line * (semibar_lenght + 1*odd), **kwargs)
+
+    def _update_attr(self, attr_name, last_value, operator):
+        previous_value = getattr(self, attr_name, 0)
+
+        if operator == 'avg':
+            metric_seen = attr_name + '_seen'
+            seen = getattr(self, metric_seen, 1)
+            setattr(self, attr_name, last_value + (last_value - previous_value) / seen)
+            setattr(self, metric_seen, seen + 1)
+        elif operator == 'sum':
+            setattr(self, attr_name, last_value + previous_value)
+        elif operator == 'last':
+            setattr(self, attr_name, last_value)
+        else:
+            raise ValueError(f'Unknowed operator {operator}')
 
     def _get_episode_text(self, episode):
         text = f"{episode+1}"
@@ -206,6 +343,7 @@ class Logger(Callback):
         return text
     
     def _get_time_text(self, dt, unit):
+        unit = 'eps' if unit == 'episode' else unit
         if dt < 1e-9:
             return f'Instant'
         if dt < 1e-6:
@@ -215,3 +353,33 @@ class Logger(Callback):
         if dt < 1:
             return f'{dt/1e-3:.01f}ms/{unit}'
         return f'{dt:.01f}s/{unit}'
+
+    @staticmethod
+    def _extract_metric_from_logs(metric_name, logs, agent_id=-1):
+
+        def _search_logs(metric_name, logs:dict):
+            if logs is None or metric_name in logs:
+                _logs = logs
+            else:
+                for _, value in logs.items():
+                    if isinstance(value, dict):
+                        _logs = _search_logs(metric_name, value)
+                        if _logs is not None:
+                            return _logs
+                _logs = None
+            return _logs
+    
+        _logs = None
+        if agent_id != -1:
+            agent_logs = logs.get(f'agent_{agent_id}')
+            if metric_name in logs:
+                _logs = logs
+            elif agent_logs is not None:
+                _logs = _search_logs(metric_name, agent_logs)
+        else:
+            _logs = _search_logs(metric_name, _logs)
+        
+        value = _logs.get(metric_name) if _logs is not None else 'N/A'
+        
+        return value
+
