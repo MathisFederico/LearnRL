591617cc2dab877cd94c22cd8d1177f3716d7f5f
MathisFederico <mathfederico@gmail.com>
2020-08-06 21:45:19 +0200

    Updated version Added Estimator, Control and Evaluation to learnrl init

--- learnrl/__init__.py
+++ learnrl/__init__.py
@@ -1,6 +1,9 @@
 # LearnRL a python library to learn and use reinforcement learning
 # Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from learnrl.core import *
+from learnrl.estimators import Estimator
+from learnrl.control import Control
+from learnrl.evaluation import Evaluation
