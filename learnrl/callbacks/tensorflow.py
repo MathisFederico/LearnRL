249305efce3cf482f64d4c025608d4c8c560efc8
Loïc M <loic.mura@student-cs.fr>
2020-08-23 17:50:23 +0200

    Conditional import for Tensorboard

---
+++ learnrl/callbacks/tensorflow.py
@@ -0,0 +1,49 @@
+import importlib
+
+tensorflow_spec = importlib.util.find_spec('tensorflow')
+
+if tensorflow_spec is not None:
+    import tensorflow as tf
+    import datetime
+
+    from learnrl.callbacks import LoggingCallback
+
+    class TensorboardCallback(LoggingCallback):
+
+        """ Tensorboard logger if tensorflow is installed. """
+
+        def __init__(self,
+                     log_dir='./logs/',
+                     ):
+            super().__init__()
+
+            self.filepath = log_dir + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            self.writer = tf.summary.create_file_writer(self.filepath)
+
+
+        def on_episode_end(self, episode, logs=None):
+            super().on_episode_end(episode, logs=logs)
+
+            with self.writer.as_default():
+                for agent_id in range(self.n_agents):
+                    for metric in self.episode_metrics:
+                        name = self._get_attr_name('episode', metric, agent_id)
+                        value = getattr(self, name, 'N/A')
+
+                        if value != 'N/A': tf.summary.scalar(name, value, step=episode + 1)
+                self.writer.flush()
+
+        def on_cycle_end(self, episode, logs=None):
+            super().on_cycle_end(episode, logs=logs)
+            if self.params['verbose'] > 1: return
+
+            with self.writer.as_default():
+                for agent_id in range(self.n_agents):
+                    for metric in self.cycle_metrics:
+                        name = self._get_attr_name('cycle', metric, agent_id)
+                        value = getattr(self, name, 'N/A')
+
+                        if value != 'N/A': tf.summary.scalar(name, value, step=episode + 1)
+
+else:
+    raise ImportError('Tensorflow should be installed.')
