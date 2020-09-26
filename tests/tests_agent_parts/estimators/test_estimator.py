65d72657304dde9478f1424c2dbbef69c4e6020d
Mathïs Fédérico <60117466+MathisFederico@users.noreply.github.com>
2020-09-27 00:35:22 +0200

    Tests/test callback/test logging callback (#67)
    * Fix typos
    
    * Add Tensorboard callback logging episode metrics
    
    * Refactoring Logger and Tensorboard callback
    
    * Add cyclic metrics to Tensorboard callback
    
    * Conditional import for Tensorboard
    
    * Fix missing arguments
    
    * Add step metrics
    
    * Add docs and run name for Tensorboard callback
    
    * Add tests and ImportError for KerasEstimator
    
    * Removed pygame for installed requirements
    
    * Removed tf.functions in KerasEstimator
    
    * Fixed wrong types in train_on_experience_batch
    
    * Added RewardHandler docstring
    Added DoneHandler docstring
    
    * Fixed display bug in Logger
    
    * Rename test file
    
    * Raise error while instanciating instead of import
    
    Allowing to import using `from learnrl.estimators import KerasEstimator`
    
    * Put import back to top
    
    * Remove duplicate code
    
    * Add blank line at the end and rename file
    
    * Fix wrong file name
    
    * Added pytest-mocker to requirements-dev
    Updated CLI with requirement-dev for tests
    
    * Removed pytest duplicate install
    
    * Fixed wrong dev-import
    
    * Fixed tests without tensorflow
    
    * Added LoggingCallback test
    Fixed LoggingCallback
    
    * Added copyright
    Removed unused test_callback
    
    Co-authored-by: Loïc M <loic.mura@student-cs.fr>
    Co-authored-by: Loïc M <muraloic0@gmail.com>

--- tests/tests_agent_parts/estimators/test_estimator.py
+++ tests/tests_agent_parts/estimators/test_estimator.py
@@ -1,3 +1,6 @@
+# LearnRL a python library to learn and use reinforcement learning
+# Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
 import pytest
 import sys
 import importlib
@@ -33,23 +36,3 @@ def test_update_learning_rate():
     if evaluation.learning_rate != expected_learning_rate:
         raise ValueError(f"Learning rate is {evaluation.learning_rate} and did not decay to {expected_learning_rate}")
 
-def test_instanciate_keras_estimator_without_tensorflow(hide_tensorflow):
-    observation_space = Discrete(4)
-    action_space = Discrete(3)
-
-    class DummyEstimator(learnrl.estimators.KerasEstimator):
-        def build(self, **kwargs):
-            pass
-
-    with pytest.raises(ImportError, match=r".*tensorflow >= 2.*"):
-        estimator = DummyEstimator(observation_space, action_space)
-
-def test_instanciate_keras_estimator_with_tensorflow():
-    observation_space = Discrete(4)
-    action_space = Discrete(3)
-
-    class DummyEstimator(learnrl.estimators.KerasEstimator):
-        def build(self, **kwargs):
-            pass
-
-    estimator = DummyEstimator(observation_space, action_space)
--- tests/tests_agents/tests_standard/test_keras.py
+++ tests/tests_agents/tests_standard/test_keras.py
@@ -7,52 +7,58 @@ from learnrl.estimators.tensorflow import KerasEstimator
 from learnrl.agents import StandardAgent
 
 from gym import spaces
-import tensorflow as tf
 
-@pytest.mark.slow
-def test_fit():
-    class MyEstimator(KerasEstimator):
+import importlib
+tensorflow_spec = importlib.util.find_spec('tensorflow')
 
-        def build(self):
-            self.model = tf.keras.experimental.LinearModel(units=self.action_size, use_bias=False)
-            self.model.compile(tf.keras.optimizers.SGD(learning_rate=self.learning_rate), loss='mse')
+if tensorflow_spec is not None:
+    from learnrl.estimators import KerasEstimator
+    import tensorflow as tf
 
-        def preprocess(self, observations, actions):
-            return tf.one_hot(observations, self.observation_size, on_value=1, off_value=0)
-    
-    observation_size, action_size = 3, 2
-    observations = np.array([0, 2, 0, 1])
-    actions = np.array([0, 0, 1, 1])
-    returns = np.array([1, -1, 1, -1], dtype=np.float32)
+    @pytest.mark.slow
+    def test_fit():
+        class MyEstimator(KerasEstimator):
 
-    estimator = MyEstimator(spaces.Discrete(observation_size), spaces.Discrete(action_size), learning_rate=len(observations))
-    estimator.fit(observations, actions, returns)
+            def build(self):
+                self.model = tf.keras.experimental.LinearModel(units=self.action_size, use_bias=False)
+                self.model.compile(tf.keras.optimizers.SGD(learning_rate=self.learning_rate), loss='mse')
 
-    expected_weights = np.array([[1, 1], [0, -1],[-1, 0]], dtype=np.float32)
-    new_returns = estimator.predict(observations).numpy()[np.arange(len(actions)), actions]
+            def preprocess(self, observations, actions):
+                return tf.one_hot(observations, self.observation_size, on_value=1, off_value=0)
+        
+        observation_size, action_size = 3, 2
+        observations = np.array([0, 2, 0, 1])
+        actions = np.array([0, 0, 1, 1])
+        returns = np.array([1, -1, 1, -1], dtype=np.float32)
 
-    print(new_returns, returns)
-    assert np.allclose(new_returns, returns)
-    print(estimator.model.weights[0].numpy(), expected_weights)
-    assert np.allclose(estimator.model.weights[0].numpy(), expected_weights)
+        estimator = MyEstimator(spaces.Discrete(observation_size), spaces.Discrete(action_size), learning_rate=len(observations))
+        estimator.fit(observations, actions, returns)
 
-@pytest.mark.slow
-def test_keras_pipeline():
-    class MyEstimator(KerasEstimator):
+        expected_weights = np.array([[1, 1], [0, -1],[-1, 0]], dtype=np.float32)
+        new_returns = estimator.predict(observations).numpy()[np.arange(len(actions)), actions]
 
-        def build(self):
-            self.model = tf.keras.models.Sequential()
-            self.model.add(tf.keras.layers.Dense(self.observation_size, activation='relu', input_shape=self.observation_shape))
-            self.model.add(tf.keras.layers.Dense(self.action_size))
+        print(new_returns, returns)
+        assert np.allclose(new_returns, returns)
+        print(estimator.model.weights[0].numpy(), expected_weights)
+        assert np.allclose(estimator.model.weights[0].numpy(), expected_weights)
 
-            self.model.compile(tf.keras.optimizers.Adam(learning_rate=self.learning_rate), loss='mse')
+    @pytest.mark.slow
+    def test_keras_pipeline():
+        class MyEstimator(KerasEstimator):
 
-        def preprocess(self, observations, actions):
-            return observations
+            def build(self):
+                self.model = tf.keras.models.Sequential()
+                self.model.add(tf.keras.layers.Dense(self.observation_size, activation='relu', input_shape=self.observation_shape))
+                self.model.add(tf.keras.layers.Dense(self.action_size))
 
-    env = CatchEnv()
-    custom_action_value = MyEstimator(observation_space=env.observation_space, action_space=env.action_space, batch_size=64, freezed_steps=20)
-    agent = StandardAgent(observation_space=env.observation_space, action_space=env.action_space, action_values=custom_action_value)
+                self.model.compile(tf.keras.optimizers.Adam(learning_rate=self.learning_rate), loss='mse')
 
-    pg = rl.Playground(env, agent)
-    pg.fit(1)
+            def preprocess(self, observations, actions):
+                return observations
+
+        env = CatchEnv()
+        custom_action_value = MyEstimator(observation_space=env.observation_space, action_space=env.action_space, batch_size=64, freezed_steps=20)
+        agent = StandardAgent(observation_space=env.observation_space, action_space=env.action_space, action_values=custom_action_value)
+
+        pg = rl.Playground(env, agent)
+        pg.fit(1)
