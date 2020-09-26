d27a1314b25399354ff2dd8f5e415777635d4763
MathisFederico <mathfederico@gmail.com>
2020-09-27 00:23:16 +0200

    Added copyright Removed unused test_callback

--- tests/tests_callbacks/test_logging.py
+++ tests/tests_callbacks/test_logging.py
@@ -1,3 +1,6 @@
+# LearnRL a python library to learn and use reinforcement learning
+# Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
 import pytest
 
 from learnrl.callbacks import LoggingCallback, Callback, CallbackList
