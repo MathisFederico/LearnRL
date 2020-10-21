edfec836236bf4f8771288da697eb39ce5ae67d3
tanweeralii <tanweerali908@gmail.com>
2020-10-22 02:28:59 +0530

    Removed circular import error

--- learnrl/agent.py
+++ learnrl/agent.py
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-from learnrl.memory import Memory
-=======
-
->>>>>>> e8418a7ba8405219567800c97244293b9e6df5a5
-
 class Agent():
 
     """ A general structure for reinforcement learning agents    
     
-<<<<<<< HEAD
     It uses by default a :class:`~learnl.memory.Memory`
-=======
-    It uses by default a :class:`Memory`
->>>>>>> e8418a7ba8405219567800c97244293b9e6df5a5
 
     Attributes
     ----------
 
         name: :class:`str`
-            The Agent's name
-<<<<<<< HEAD
         memory: :class:`~learnl.memory.Memory`
-=======
-        memory: :class:`Memory`
->>>>>>> e8418a7ba8405219567800c97244293b9e6df5a5
             The Agent's memory
     
     """
@@ -53,11 +38,7 @@ class Agent():
         raise NotImplementedError
 
     def remember(self, observation, action, reward, done, next_observation=None, info={}, **param):
-<<<<<<< HEAD
         """ Uses the agent's :class:`~learnl.memory.Memory` to remember experiences
-=======
-        """ Uses the agent's :class:`Memory` to remember experiences
->>>>>>> e8418a7ba8405219567800c97244293b9e6df5a5
         
         Often, the agent will use a |hash| to store observations efficiently
 
@@ -70,8 +51,3 @@ class Agent():
             ...                       info, **param)
         """
         raise NotImplementedError
-<<<<<<< HEAD
-    
-=======
-    
->>>>>>> e8418a7ba8405219567800c97244293b9e6df5a5
