6a64ad02fa03c692ad03253d4a00446c323a4635
Mathïs Fédérico <60117466+MathisFederico@users.noreply.github.com>
2020-11-12 06:42:31 +0100

    Release/v0.2.1 (#87)
    * Removed main.py
    
    * Added copyright in heads
    
    * Updated version

--- learnrl/agent.py
+++ learnrl/agent.py
@@ -1,3 +1,6 @@
+# LearnRL a python library to learn and use reinforcement learning
+# Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
 class Agent():
 
     """ A general structure for reinforcement learning agents    
