617968e4f51648fcfa265def142bcf53f8fe2510
MathisFederico <mathfederico@gmail.com>
2020-11-12 06:28:31 +0100

    Fixed tensorflow test

--- tests/tests_callbacks/test_tensorboard.py
+++ tests/tests_callbacks/test_tensorboard.py
@@ -9,10 +9,10 @@ tensorflow_spec = importlib.util.find_spec('tensorflow')
 if tensorflow_spec is not None:
     def test_instanciate_without_tensorflow(hide_tensorflow):
         with pytest.raises(ImportError, match=r".*tensorflow >= 2.*"):
-            callback = learnrl.callbacks.TensorboardCallback()
+            callback = learnrl.callbacks.TensorboardCallback(log_dir='logs')
 
     def test_instanciate_with_tensorflow():
-        callback = learnrl.callbacks.TensorboardCallback()
+        callback = learnrl.callbacks.TensorboardCallback(log_dir='logs')
 else:
     pass
 
