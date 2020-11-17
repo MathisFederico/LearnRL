e5daa3036454bd8d934791170480341542f1be41
Loïc M <muraloic0@gmail.com>
2020-11-17 10:25:06 +0100

    Clean python file example

--- examples/cartpole/cartpole.py
+++ examples/cartpole/cartpole.py
@@ -1,16 +1,27 @@
-import learnrl as rl
-from learnrl.agents import StandardAgent, RandomAgent
-from learnrl.estimators.tensorflow import KerasEstimator
-# from learnrl.callbacks.tensorflow import TensorboardCallback
 import gym
-
 import numpy as np
 
-from tensorflow.keras import layers, optimizers, Input
+import learnrl as rl
+from learnrl.agents import StandardAgent
+
+from tensorflow.keras import layers, models, optimizers
 from tensorflow.keras.models import Sequential
 
 # Load the environment
 env = gym.make('CartPole-v1')
+
+# Define the model for our action-value estimator
+class MyEstimator(rl.estimators.KerasEstimator):
+    def preprocess(self, observations, actions):
+        return observations
+
+    def build(self, **kwargs):
+        self.model = models.Sequential()
+        self.model.add(layers.Flatten(input_shape=self.observation_shape))
+        self.model.add(layers.Dense(self.observation_size, activation='tanh'))
+        self.model.add(layers.Dense(self.action_size, activation='linear'))
+
+        self.model.compile(optimizers.Adam(learning_rate=self.learning_rate), loss='mse')
 
 # Define a reward scaler to avoid huge reward (more difficult to predict)
 class RewardScaler(rl.RewardHandler):
@@ -19,33 +30,15 @@ class RewardScaler(rl.RewardHandler):
 
 reward_handler = RewardScaler();
 
-# Define the model for our Estimator
-class ActionValues(KerasEstimator):
-    def preprocess(self, observations, actions):
-        return observations
-
-    def build(self, **kwargs):
-        print(self.observation_size)
-        print(self.action_size)
-
-        model = Sequential()
-        model.add(Input(shape=(self.observation_size,)))
-        model.add(layers.Dense(self.observation_size, activation='tanh', kernel_initializer='random_normal', bias_initializer='zeros'))
-        model.add(layers.Dense(self.action_size, activation='linear', kernel_initializer='random_normal', bias_initializer='zeros'))
-
-        model.summary()
-        model.compile(optimizers.Adam(self.learning_rate, decay=1e-6), loss='mse')
-
-        self.model = model
 
 # Let's construct our agent !
-custom_action_values = ActionValues(env.observation_space,
-                                    env.action_space,
-                                    freezed_steps=200,
-                                    learning_rate=1e-3,
-                                    batch_size=32,
-                                    epochs_per_step=1,
-                                    learning_rate_decay=0)
+custom_action_values = MyEstimator(env.observation_space,
+                                   env.action_space,
+                                   freezed_steps=200,
+                                   learning_rate=1e-3,
+                                   batch_size=32,
+                                   epochs_per_step=1,
+                                   learning_rate_decay=0)
 
 agent = StandardAgent(observation_space=env.observation_space,
                       action_space=env.action_space,
@@ -56,12 +49,13 @@ agent = StandardAgent(observation_space=env.observation_space,
                       memory_len=10000)
 
 # Let him learn
-pg = rl.Playground(env, agent)
-pg.run(1000,
-       verbose=1,
-       render=False,
-       cycle_len=25,
-       learn=True,
-       reward_handler=reward_handler,
-       # callbacks=[TensorboardCallback(log_dir='./logs_test/', run_name='test_model')]
-       )
+playground = rl.Playground(env, agent)
+
+# Train for 500 episodes
+playground.fit(500,
+               verbose=1,
+               reward_handler=reward_handler,
+               callbacks=[]) # Add Tensorboard, wandb or your own callbacks here
+
+# Show its performance for 5 episodes
+playground.test(5, verbose=1)
