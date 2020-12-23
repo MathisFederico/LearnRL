36e2a428ceaff0683a6e4490b98761bfaf3c4401
MathisFederico <mathfederico@gmail.com>
2020-12-23 04:17:49 +0100

    Removed environments

--- learnrl/environments/single_agents/__init__.py
+++
@@ -1,5 +0,0 @@
-# LearnRL a python library to learn and use reinforcement learning
-# Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
-
-from learnrl.environments.single_agents.random_nim import RdNimEnv
-from learnrl.environments.single_agents.catch import CatchEnv
