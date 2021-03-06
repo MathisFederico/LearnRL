75ebe132fcb26638b86e6bc4ae6b317650709cbd
MathisFederico <mathfederico@gmail.com>
2021-03-06 18:04:00 +0100

    Added TensorboardCallback test docstring

--- tests/unit/callbacks/test_tensorboard.py
+++ tests/unit/callbacks/test_tensorboard.py
@@ -7,11 +7,16 @@ import importlib
 tensorflow_spec = importlib.util.find_spec('tensorflow')
 
 if tensorflow_spec is not None:
-    def test_instanciate_without_tensorflow(hide_tensorflow):
-        with pytest.raises(ImportError, match=r".*tensorflow >= 2.*"):
-            learnrl.callbacks.TensorboardCallback(log_dir='logs')
 
-    def test_instanciate_with_tensorflow():
-        learnrl.callbacks.TensorboardCallback(log_dir='logs')
+    class TestTensorboardCallback():
+
+        def test_instanciate_without_tensorflow(self, hide_tensorflow):
+            """Should raise ImportError if tensorflow is not found"""
+            with pytest.raises(ImportError, match=r".*tensorflow >= 2.*"):
+                learnrl.callbacks.TensorboardCallback(log_dir='logs')
+
+        def test_instanciate_with_tensorflow(self):
+            """Should instanciate correctly tensorflow is found"""
+            learnrl.callbacks.TensorboardCallback(log_dir='logs')
 else:
     pass
