3f01bf2a6b5951665502f1503a8f8d95f758fe6e
Loïc M <loic.mura@student-cs.fr>
2021-03-06 21:13:54 +0100

    Remove pytest slow marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,29 +7,6 @@ import importlib
 import pytest
 import learnrl
 
-def pytest_addoption(parser):
-    """ Add options to pytest CLI """
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_configure(config):
-    """ Register new markers """
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
-
-
-def pytest_collection_modifyitems(config, items):
-    """ Skip tests according to slow parameter """
-
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
-
 @pytest.fixture
 def reload_tensorflow():
     """ Reload tensorflow after hiding it from the modules list """
