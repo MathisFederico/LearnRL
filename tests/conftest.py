67418b42e1f5e6e10978ab7be2cec226b975dbe5
MathisFederico <mathfederico@gmail.com>
2021-03-06 18:03:34 +0100

    Removed pytest slow marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,10 @@
 # LearnRL a python library to learn and use reinforcement learning
 # Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
+import os
 import pytest
 import importlib
 import learnrl
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
 
 @pytest.fixture
 def reload_tensorflow():
@@ -47,4 +29,3 @@ def hide_wandb(reload_wandb, mocker):
     mocker.patch.dict('sys.modules', { 'wandb': None })
     importlib.reload(learnrl.callbacks)
     importlib.reload(learnrl)
-
