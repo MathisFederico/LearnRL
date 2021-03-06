9e2b57cb1e790f3e78ce833eadb7c04518d046ba
MathisFederico <mathfederico@gmail.com>
2021-03-06 18:04:13 +0100

    Added WandbCallback tests docstring

--- tests/unit/callbacks/test_wandb.py
+++ tests/unit/callbacks/test_wandb.py
@@ -7,11 +7,16 @@ import importlib
 wandb_spec = importlib.util.find_spec('wandb')
 
 if wandb_spec is not None:
-    def test_instanciate_without_wandb(hide_wandb):
-        with pytest.raises(ImportError, match=r".*wandb >= 0.10.*"):
-            learnrl.callbacks.WandbCallback()
 
-    def test_instanciate_with_wandb():
-        learnrl.callbacks.WandbCallback()
+    class TestWandbCallback():
+
+        def test_instanciate_without_wandb(self, hide_wandb):
+            """Should raise ImportError if wandb is not found"""
+            with pytest.raises(ImportError, match=r".*wandb >= 0.10.*"):
+                learnrl.callbacks.WandbCallback()
+
+        def test_instanciate_with_wandb(self):
+            """Should instanciate correctly wandb is found"""
+            learnrl.callbacks.WandbCallback()
 else:
     pass
