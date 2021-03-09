710b0598ebfae0de6ff8e4a52bd88add9c8ec68e
MathisFederico <mathfederico@gmail.com>
2021-03-09 17:09:42 +0100

    Changed version to 1.0.0

--- learnrl/__init__.py
+++ learnrl/__init__.py
@@ -1,9 +1,9 @@
 # LearnRL a python library to learn and use reinforcement learning
 # Copyright (C) 2020 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-"""LearnRL a python library to learn and use reinforcement learning"""
+"""LearnRL a python library to learn and log reinforcement learning"""
 
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 
 from learnrl.agent import Agent
 from learnrl.envs import TurnEnv
